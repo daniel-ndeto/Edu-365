025915582d3fcdbccfc7883c36ddbb622ec88a05
daniel-ndeto <danielndeto222@gmail.com>
2025-04-01 17:18:57 +0300

    index file updates

--- backend/index.js
+++ backend/index.js
@@ -2,44 +2,41 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-// const bodyParser = require("body-parser")
 const app = express();
 const Routes = require("./routes/route.js");
 
-const PORT = process.env.PORT || 5000;
-
 dotenv.config();
 
-// apParser.urlencoded({ limit: '10mb', extended: true }))
+const PORT = process.env.PORT || 5000;
 
+// Middlewares
 app.use(express.json({ limit: "10mb" }));
-app.use(cors(
-  {
-    origin: "https://edu-365-front.vercel.app",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
-  }
-));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
-mongoose.connect('mongodb+srv://daniel-ndeto:dan%4040122129@cluster0.fdlst.mongodb.net/edu-365?retryWrites=true&w=majority',)
+// Configure CORS to allow requests from your frontend domain
+app.use(cors({
+  origin: "https://edu-365-front.vercel.app",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 
+// Connect to MongoDB using the connection string from environment variables
+mongoose.connect(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log("NOT CONNECTED TO NETWORK", err));
+
+// Root route
 app.get("/", (req, res) => {
   res.send("Welcome to the server");
 });
 
-app.use(express.urlencoded(
-));
-
-mongoose
-  .connect(process.env.MONGO_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(console.log("Connected to MongoDB"))
-  .catch((err) => console.log("NOT CONNECTED TO NETWORK", err));
-
+// Use defined routes
 app.use("/", Routes);
 
+// Start the server
 app.listen(PORT, () => {
   console.log(`Server started at port no. ${PORT}`);
 });
