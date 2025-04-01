26b474dd01eb2dc856f913dc6965eff24e911d00
daniel-ndeto <danielndeto222@gmail.com>
2025-04-01 16:40:07 +0300

    editing errors

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ dotenv.config();
 app.use(express.json({ limit: "10mb" }));
 app.use(cors(
   {
-    origin: "https://edu-365-front.vercel.app/",
+    origin: "https://edu-365-front.vercel.app",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   }
